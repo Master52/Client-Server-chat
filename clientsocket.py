0399e91b5bad9d0dbbcbc8a502591b91eaa3f750
Master52 <harajmast9008@gmail.com>
2018-12-14 17:28:24 +0530

    Changed UI

--- clientsocket.py
+++ clientsocket.py
@@ -1,8 +1,10 @@
 #Client/Server using mult threading
+
 import socket
 import sys
 import time
 import threading
+from colorama import Style,Fore
 
 class ClientSocket(object):
     def __init__(self,addr=socket.gethostbyname(socket.gethostname()),port = 5253,debug = True):
@@ -10,29 +12,41 @@ class ClientSocket(object):
         self.port = port
         self.debug = debug
         self.connection_is_alive = False #For checking wether the server is still alive or not
+
+        #COLOR FOR PRETTY DISPLAY
+        self.green = Fore.GREEN
+        self.reset = Style.RESET_ALL
+        self.bright = Style.BRIGHT
+        self.blue = Fore.BLUE
+        self.red = Fore.RED
+        self.yellow = Fore.YELLOW
+
     def start_connection(self) :
         self.clientfd = socket.socket()
         if self.debug == True:
-              print("Socket created succefullt")
+              print(self.green + self.bright + "Socket created succefullt" + self.reset )
 
-        print("Establishing connection")
+        print(self.blue + self.bright + "Establishing connection...." + self.reset)
+
         try:
             self.clientfd.connect((self.addr,self.port))
         except ConnectionRefusedError:
             #If server is not fount it will try to reconnect after 5 sec.
-            print("Cannot find server.\n trying to reconnect..")
+            print(self.red + self.bright + "Cannot find server.\n trying to reconnect.." + self.reset)
             time.sleep(5)
             self.start_connection()
+
         self.connection_is_alive = True
         self.startChatting()
 
     def _sendmsg(self):
         try:
             while self.connection_is_alive:
-              print("\n[ME:]   ",end='')
-              msg = input()
+              print(self.blue + self.bright + "\n[ME:]   ",end=''+ self.reset)
+              msg = input(self.yellow + self.bright)
             #Always checking wether the server is still alive or not
               if self.connection_is_alive:
+
                     if msg == 'q' or msg == 'Q':
                         self.connection_is_alive = False
                         self.clientfd.send(bytes(('close'),'utf-8'))
@@ -40,6 +54,7 @@ class ClientSocket(object):
                     self.clientfd.send(bytes(msg,'utf-8'))
               else:
                     return
+
         except OSError:
             print(OSError)
 
@@ -50,17 +65,20 @@ class ClientSocket(object):
                  msg = msg.decode(encoding = 'utf-8')
                  if self.connection_is_alive:
                      if msg == 'close':
-                         print("Server is down ")
+                         print(self.red + self.bright +"Server is down " + self.reset)
                          self.connection_is_alive = False
                          break
-                     print("\n[Client:]    {0}".format(msg))
+                     print(self.blue + self.bright+"\n[Client:]" +"  {0}".format(self.green + msg + self.reset))
+
         except OSError:
             return
+
     def startChatting(self):
         #Again starting two threads one will recivie the message 
         #The other will send the message
         self.t1 = threading.Thread(target = self._recvmsg,args=())
         self.t2 = threading.Thread(target = self._sendmsg,args=())
+
         self.t1.start()
         self.t2.start()
         self.t1.join()
@@ -73,14 +91,18 @@ class ClientSocket(object):
             self.clientfd.send(bytes(('close'),'utf-8'))
             #Stops the infinite loop of all the threads
             self.connection_is_alive = False
-        print("\nPress enter to Quit")
+        print(self.blue + self.bright + "\nPress enter to Quit" + self.reset)
 
     def __del__(self):
         #Closes the connection
         self.clientfd.close()
         if self.debug == True :
-            print("Connection closed succefully")
-        
+            print(self.green + self .bright + "Connection closed succefully" + self.reset)
+        del self.clientfd
+
+        del self.yellow,self.green,self.blue,self.red
+        del self.bright,self.reset
+
         del self.debug
 
 
@@ -91,6 +113,7 @@ def main():
        del c
     #If the user presses C-c to c:w
     # loes  the chat
+
     except KeyboardInterrupt:
          c.closeconnection()
 
--- serverSocket.py
+++ serverSocket.py
@@ -3,7 +3,7 @@
 import socket
 import sys
 import threading
-
+from colorama import Style,Fore,Back
 
 
 class Socket(object):
@@ -13,6 +13,14 @@ class Socket(object):
         self.debug = debug # For debugging purose
         self.connectionCount = 0 # Number of client getting connected
         self.connectionDetail = {} # Hold detail of each client such as address , file descriptor(fd)
+
+        #COLOR FOR PRETTY DISPLAY
+        self.green = Fore.GREEN
+        self.reset = Style.RESET_ALL
+        self.bright = Style.BRIGHT
+        self.blue = Fore.BLUE
+        self.red = Fore.RED
+        self.yellow = Fore.YELLOW
     
     
 
@@ -21,24 +29,25 @@ class Socket(object):
         self._serverSocket = socket.socket()
         try :
             if self.debug == True :
-                print("Socket Created")
+                print(self.green + self.bright +"Socket Created"+ self.reset)
             self._serverSocket.bind((self.addr,self.port))
+
             if self.debug == True:
                print("Socket Binded succefully")
+
             while True:
-                print("{0} Listening for connection on port{1} ...".format(self.addr,self.port))
+                print(self.yellow + self.bright +"{0} Listening for connection on port {1} ...".format(self.addr,self.port)+self.reset)
                 self._serverSocket.listen(5)
                 clientfd,addr = self._serverSocket.accept()
-                print("Connected to host {0}".format(addr[0]))
-
+                print(self.blue + self.bright +"Connected to host {0}".format(addr[0]) + self.reset)
                # 'is_alive' parameter is used to check wether the client is alive or not 
                 self.connectionDetail[addr] = {'sockd':clientfd ,'is_alive':True}
                 self.connectionCount += 1
                 # A Thread which will handle every new client get connected 
                 threading.Thread(target = self.newConnectionHandler,args=(addr,)).start()
+
         except OSError:
-            print("Another server is running on the same port number")
-            
+            print(self.red + self.bright +"Another server is running on the same port number" + self.reset)
     
     def newConnectionHandler(self,addr):
         self.startChating(addr)
@@ -46,19 +55,21 @@ class Socket(object):
     def _sendmsg(self,addr):
         try:
             while self.connectionDetail[addr]['is_alive']:
-                 print("\n[Server:]    ",end='')
-                 msg = input()
+                 print(self.blue + self.bright +"\n[Server:] ",end=''+ self.reset)
+                 msg = input(self.yellow + self.bright)
                  #Always check wether the client is online or not before send the message
                  #If it will break the while loop
                  if self.connectionDetail[addr]['is_alive']:
+
                      if msg == 'q' or msg == 'Q':
                         self.connectionDetail[addr]['sockd'].send(bytes('close','utf-8'))
                         self.connectionDetail[addr]['is_alive'] = False
                         break
+                    
                      self.connectionDetail[addr]['sockd'].send(bytes(msg,'utf-8'))
+
                  else:
                     break
-        
             self.disconnect(addr)
 
         except OSError:
@@ -76,8 +87,9 @@ class Socket(object):
              if msg == 'close':
                  self.connectionDetail[addr]['sockd'].send(bytes('close','utf-8'))
                  break
+
              else:
-                print("\n{0}    {1}".format(addr[0],msg))
+                print(self.blue + self.bright +"\n[{0}] ".format(addr[0]) + self.green + "{0}".format(msg) + self.reset)
         #Comming out from this loop simply mean the client has been disconnected so it call disconnects method to remove it
 
         self.disconnect(addr)
@@ -87,6 +99,7 @@ class Socket(object):
         #The other will recivies the messsage
         t1 = threading.Thread(target=self._sendmsg,args=(addr,))
         t2 = threading.Thread(target=self._recvmsg,args=(addr,))
+        
         t1.start()
         t2.start()
 
@@ -95,13 +108,14 @@ class Socket(object):
         #If no address is pass that means all the connection should be disconnected
         if address == None:
             for addr,value in self.connectionDetail.items():
+
                 if value['is_alive'] == True: #Checking wether the client is still online or not
                    value['sockd'].send(bytes(('close'),'utf-8'))
                    value['sockd'].close()
                    value['is_alive'] = False
                    self.connectionCount -= 1
-                   print("{0} disconnected".format(addr))
-                   print("Press Enter to quit")
+                   print(self.red + self.bright +"{0} disconnected".format(addr) + self.reset)
+                   print(self.red + self.bright +"Press Enter to quit" + self.reset)
         else:
             #checking wether the address is present in client list or not
             if address in self.connectionDetail:
@@ -111,17 +125,21 @@ class Socket(object):
                     self.connectionDetail[address]['is_alive'] = False
                     del(self.connectionDetail[address])
                     self.connectionCount -= 1
-                    print("{0} disconnected".format(address))
+                    print(self.red + self.bright + "{0} disconnected".format(address) + self.reset)
 
 
     def __del__(self) :
         #closing the server socket
         self._serverSocket.close()
-        del self.connectionCount
-        del self.connectionDetail
-        del self.addr
+
         if self.debug == True:
-            print("Socket closed succefully")
+            print(self.green + self.bright + "Socket closed succefully" + self.reset)
+
+        del self.connectionCount,self.addr
+        del self.connectionDetail
+
+        del self.green,self.red,self.yellow,self.blue
+        del self.bright,self.reset
 
 
 
